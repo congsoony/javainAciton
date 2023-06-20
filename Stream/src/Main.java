2794f5a0e38e6d881a0ba15326f1d5cf6e3e12fa
leejaseop <vavonae@gmail.com>
2023-06-20 19:02:50 +0900

    스트림 구조 실습

---
+++ Stream/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class Main {
+
+    public static List<Dish> menu = Item.menu;
+    public static void main(String[] args) {
+
+        List<String> threeHighCaloricDishNames = menu.stream().filter(dish -> dish.getCalories()>300)
+                .map(Dish::getName)
+                .limit(3)//선착순 3개까지만
+                .collect(Collectors.toList());
+        System.out.println(threeHighCaloricDishNames);
+
+
+
+        //게으른 특성확인
+        List<String> names = menu.stream().filter(dish->{
+            System.out.println("filtering: "+dish.getName());
+            return dish.getCalories()>300;})
+                .map(dish->{
+                    System.out.println("mapping: "+dish.getName());
+                    return dish.getName();
+                })
+                .limit(3)
+                .collect(Collectors.toList());
+        //filtering: pork
+        //mapping: pork
+        //filtering: beef
+        //mapping: beef
+        //filtering: chicken
+        //mapping: chicken
+        System.out.println(names);
+
+
+
+    }
+}
