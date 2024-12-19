7929af1ce2fd99308d0ca9581cfbf886d31cb834
leejaseop <vavonae@gmail.com>
2024-12-19 12:23:18 +0900

    OptionalTest Duration 클래스 테스트 완료

--- Date/src/DurationTest.java
+++ Date/src/DurationTest.java
@@ -1,13 +1,25 @@
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class DurationTest {
     public static void main(String[] args) {
 
-        Period tenDays =Period.between(LocalDate.of(2017,9,11),
-                LocalDate.of(2017,8,12));
-        System.out.println(tenDays.getDays());
+//        Period tenDays =Period.between(LocalDate.of(2017,9,11),
+//                LocalDate.of(2017,8,12));
+//        System.out.println(tenDays.getDays());
+
+        Duration threeMinutes = Duration.of(3, ChronoUnit.MINUTES);
+        System.out.println(threeMinutes);
+
+        Period tenDays = Period.ofDays(10);
+        Period threeWeeks = Period.ofWeeks(3);
+        Period twoYearSixMonthOneDay = Period.of(2,6,1);
+        System.out.println(tenDays);
+        System.out.println(threeWeeks);
+        System.out.println(twoYearSixMonthOneDay);
 
     }
 }
--- Optional/src/Main.java
+++ Optional/src/Main.java
@@ -1,5 +1,7 @@
 import javax.swing.text.html.Option;
 import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.Properties;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -11,7 +13,23 @@ public class Main {
         String name = optPerson.flatMap(Person::getCar).flatMap(Car::getInsurance)
                 .map(Insurance::getName)
                 .orElse("Unknow");
+
+
+
     }
+    public static int readDuation(Properties props, String name){
+        String value = props.getProperty(name);
+        if(value!=null){
+            try{
+                int i= Integer.parseInt(value);
+                if(i>0){
+                    return i;
+                }
+            }
+            catch (NumberFormatException e){}
+
+        }
+        return 0;
 }
 class Person{
     private Optional<Car> car;
