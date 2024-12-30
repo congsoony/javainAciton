02121a3e8a6ec7e13a7173b27b537faeeed28712
leejaseop <vavonae@gmail.com>
2024-12-30 20:14:17 +0900

    ScheduleExecutor를 이용한 thread 이용한법

---
+++ part15/src/ScheduledExecutorServiceExample.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledExecutorServiceExample {
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+
+        work1();
+        scheduledExecutorService.schedule(
+                ScheduledExecutorServiceExample::work2,10, TimeUnit.SECONDS
+        );
+        scheduledExecutorService.shutdown();
+
+
+    }
+    public static void work1(){
+        System.out.println("hello from work1!");
+    }
+    public static void work2(){
+        System.out.println("Hello from Work2!");
+    }
+}
