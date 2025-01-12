a8d8d2da90fb73acc365b9f9c99409b5e60fa7b6
leejaseop <vavonae@gmail.com>
2025-01-12 16:40:18 +0900

    일부러 에러를 내지않으면 스택오버플로우 ,, 스택오버플로우를 제거하기위한 Executor 해결

--- part17/src/TempSubscription.java
+++ part17/src/TempSubscription.java
@@ -1,9 +1,11 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Flow.*;
 
 public class TempSubscription implements Subscription {
     private final Subscriber<? super TempInfo> subscriber;
     private final String town;
-
+    private static final ExecutorService executor = Executors.newSingleThreadExecutor();
     public TempSubscription(Subscriber<? super TempInfo> subscriber, String town) {
         this.subscriber = subscriber;
         this.town = town;
@@ -16,14 +18,18 @@ public class TempSubscription implements Subscription {
 
     @Override
     public void request(long n) {
-        for(long i=0L;i<n;i++){
-            try{
-                subscriber.onNext(TempInfo.fetch(town));
-            }catch (Exception e){
-                subscriber.onError(e);
-                break;
+        executor.submit(()->{
+            for(long i =0L;i<n;i++){
+                try{
+                    subscriber.onNext(TempInfo.fetch(town));
+                }catch (Exception e){
+                    subscriber.onError(e);
+                    break;
+                }
             }
-        }
+        });
+
+
     }
 
 }
