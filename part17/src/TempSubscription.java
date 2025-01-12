36ae4d9c51f942f5def52b40a5f02061c7a5dcf8
leejaseop <vavonae@gmail.com>
2025-01-12 16:14:01 +0900

    TempInfo 스트림을 전송하는 Subscription

---
+++ part17/src/TempSubscription.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.Flow.*;
+
+public class TempSubscription implements Subscription {
+    private final Subscriber<? super TempInfo> subscriber;
+    private final String town;
+
+    public TempSubscription(Subscriber<? super TempInfo> subscriber, String town) {
+        this.subscriber = subscriber;
+        this.town = town;
+    }
+
+    @Override
+    public void cancel() {
+        subscriber.onComplete();
+    }
+
+    @Override
+    public void request(long n) {
+        for(long i=0L;i<n;i++){
+            try{
+                subscriber.onNext(TempInfo.fetch(town));
+            }catch (Exception e){
+                subscriber.onError(e);
+                break;
+            }
+        }
+    }
+
+}
